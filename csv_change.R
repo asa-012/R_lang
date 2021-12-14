633b36f9c54d4523643fc0b861ff2c6dfc904d18
asa-012 <yjdyj.20356299@gmail.com>
2021-12-15 03:41:52 +0900

    add:順位

--- csv_change.R
+++ csv_change.R
@@ -5,7 +5,19 @@ english <- rdf[, "英語"]
 math <- rdf[, "数学"]
 physics <- rdf[, "物理"]
 chemistry <- rdf[, "化学"]
-sum <- c(japanese + english + math + physics + chemistry)
-sum
-avg <- c(sum / 5)
-avg
+#5教科合計点
+sumScore <- c(japanese + english + math + physics + chemistry)
+sumScore
+#平均点
+avgScore <- c(sum / 5)
+avgScore
+#順位を求める　合計点から 点数の大きい順にソート
+rank1 <- rank(sumScore,ties.method = "min")
+rank1
+rank2 <- c(51-rank1)
+rank2
+#偏差値=(点数-平均点)*10/標準偏差+50
+avgall <-c(sum(sumScore)/50)
+avgall
+
+# A>65,55<=B<65,55<=C<45,45<=D<35,35>E
