73ce3e5876bc3ab47a268388bd24db923ae9369f
asa-012 <yjdyj.20356299@gmail.com>
2021-12-15 04:26:02 +0900

    add:計算後に挿入処理追加

--- csv_change.R
+++ csv_change.R
@@ -12,10 +12,10 @@ sumScore
 avgScore <- c(sum / 5)
 avgScore
 #順位を求める　合計点から 点数の大きい順にソート
-rank1 <- rank(sumScore, ties.method = "min")
-rank1
-rank2 <- c(51 - rank1)
-rank2
+rank <- rank(sumScore, ties.method = "min")
+rank
+rank <- c(51 - rank1)
+rank
 #偏差値=(点数-平均点)*10/標準偏差+50
 sumAllSubject <- sum(sumScore)
 sumAllSubject
@@ -25,14 +25,38 @@ avgAllPeople
 
 #標準偏差
 sd <- c(sumScore - avgAllPeople)
-sd <- sd^2
-sd <-sum(sd)
-sd <- c(sd/50)
+sd <- sd ^ 2
+sd <- sum(sd)
+sd <- c(sd / 50)
 sd <- c(sqrt(sd))
 sd
 
 #偏差値=(点数-平均点)*10/標準偏差+50
+devValue <- c(sum - avgAllPeople)
+devValue <- c(devValue * 10)
+devValue <- c(devValue / sd)
+devValue <- c(devValue + 50)
 
-
+rdf$sum <- sumScore
+rdf$avg<- avgAllPeople
+rdf$rank <- rank
+rdf$devValue <- devValue
 
 # A>65,55<=B<65,55<=C<45,45<=D<35,35>E
+
+judge <- vector(length = 50, mode = "character")
+judge[0 <= devValue] <- "E"
+judge[35 <= devValue] <- "D"
+judge[45 <= devValue] <- "C"
+judge[55 <= devValue] <- "B"
+judge[65 <= devValue] <- "A"
+
+judge
+
+rdf$judge <- judge
+
+rdf
+
+
+
+
