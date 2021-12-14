21c2e7b374f1b79a457e06168d93385d61d317af
asa-012 <yjdyj.20356299@gmail.com>
2021-12-15 03:21:16 +0900

    add:合計を計算

--- csv_change.R
+++ csv_change.R
@@ -1,8 +1,10 @@
 rdf <- read.csv(file = "seiseki.csv")
 rdf
 japanese <- rdf[,"国語"]
-japanese
 english <- rdf[,"英語"]
 math <- rdf[,"数学"]
 physics <- rdf[,"物理"]
 chemistry <- rdf[,"化学"]
+sum <- c(japanese+english+math+physics+chemistry)
+avg <- c(sum/5)
+avg
