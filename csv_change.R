d3d261b2841260de73cf81166b9f22893ec1b4e0
asa-012 <yjdyj.20356299@gmail.com>
2021-12-15 03:24:48 +0900

    add:平均を計算

--- csv_change.R
+++ csv_change.R
@@ -1,10 +1,11 @@
 rdf <- read.csv(file = "seiseki.csv")
 rdf
-japanese <- rdf[,"国語"]
-english <- rdf[,"英語"]
-math <- rdf[,"数学"]
-physics <- rdf[,"物理"]
-chemistry <- rdf[,"化学"]
-sum <- c(japanese+english+math+physics+chemistry)
-avg <- c(sum/5)
+japanese <- rdf[, "国語"]
+english <- rdf[, "英語"]
+math <- rdf[, "数学"]
+physics <- rdf[, "物理"]
+chemistry <- rdf[, "化学"]
+sum <- c(japanese + english + math + physics + chemistry)
+sum
+avg <- c(sum / 5)
 avg
