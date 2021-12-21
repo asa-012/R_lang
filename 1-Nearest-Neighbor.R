95747fa5c1a9e2be224de49d5c0fad273b108649
asa-012 <yjdyj.20356299@gmail.com>
2021-12-22 00:42:55 +0900

    add:前半を教師，後半をテスト

--- 1-Nearest-Neighbor.R
+++ 1-Nearest-Neighbor.R
@@ -28,3 +28,16 @@ niris <- iris
 niris[, 1:4] <- scale(niris[, 1:4])
 niris$Species <- as.character(niris$Species)
 niris$id <- 1:nrow(niris)
+#前半を教師，後半をテスト
+train_data_1 <-
+  niris[c(1:25, 51:75, 101:125), 1:4] # 教師データセット
+test_data_1 <-
+  niris[c(1:25, 51:75, 101:125), 1:4] # テストデータセット
+train_label_1 <-
+  niris[c(1:25, 51:75, 101:125), 5] # 教師データの分類ラベル
+test_label_1 <-
+  niris[c(1:25, 51:75, 101:125), 5] # テストデータの正解ラベル
+pred_label_1 <-
+  Pred_1NN(train_data_1, test_data_1, train_label_1)
+error_rate_1 <-
+  sum(pred_label_1 != test_label_1) / 75.0
