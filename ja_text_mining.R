c7083b5631d44c10e532b9402cffc069a4722a3e
asa-012 <yjdyj.20356299@gmail.com>
2022-01-26 15:48:16 +0900

    add:comment

--- ja_text_mining.R
+++ ja_text_mining.R
@@ -1,10 +1,12 @@
-#Q0 
+#Q0 docMatrix 関数で boj フォルダからターム・文書行列を作成する
+#プログラムを作れ ただし品詞は名詞のみで，重みは“ tf idf *norm とする
+
 bojDF0 <- docMatrix("boj", pos = c(" 名詞 "), weight = "tf*idf*norm")
 bojDF1 <- makeRNDF(bojDF0)
 
 #Q1
-
 #Q2
 #Q3
 #Q4
 #Q5
+
