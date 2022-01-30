fc9e60e6aafa2bd3e7a22827e9c82932173d1d99
asa-012 <yjdyj.20356299@gmail.com>
2022-01-31 01:58:54 +0900

    add:get co-occurrence_frequency

--- page_rank.R
+++ page_rank.R
@@ -1,23 +1,34 @@
 # 内容語の抽出
-tableAfterExtraction <- function() {
-  f <- 5
-  sarukaniDF <-
-    docDF("sarukani_gassen-utf8.txt",
-          type = 1,
-          pos = c("名詞", "形容詞", "動詞"))
-  # 降順(行番号大→小)　４列目を並び替えたもの
-  a <- sarukaniDF[order(sarukaniDF[, 4], decreasing = TRUE),]
-  # f以上のもののみを抽出しbに代入
-  b <- a[a$"sarukani_gassen-utf8.txt" >= f,]
-  c <-
-    b[(b$POS2 != "非自立") &
-        (b$POS2 != "接尾") & (b$POS2 != "特殊") & (b$POS2 != "代名詞"),]
-  d <- c$TERM
-  result <- matrix(0, nrow = length(d), ncol = length(d))
-  return(result)
-}
-table_aftere_extraction <- tableAfterExtraction(f)
+f <- 5
+sarukaniDF <-
+  docDF("sarukani_gassen-utf8.txt",
+        type = 1,
+        pos = c("名詞", "形容詞", "動詞"))
+# 降順(行番号大→小)　４列目を並び替えたもの
+a <- sarukaniDF[order(sarukaniDF[, 4], decreasing = TRUE), ]
+# f以上のもののみを抽出しbに代入
+b <- a[a$"sarukani_gassen-utf8.txt" >= f, ]
+c <-
+  b[(b$POS2 != "非自立") &
+      (b$POS2 != "接尾") & (b$POS2 != "特殊") & (b$POS2 != "代名詞"), ]
+termRow <- c$TERM
+resultExtractionTable <-
+  matrix(0, nrow = length(termRow), ncol = length(termRow))
 # 共起頻度の作成 collocateのspan=3と最小頻度f=5とする
+for (i in 1:length(d)) {
+  co_occurrence_frequency <-
+    collocate("sarukani_gassen-utf8.txt",
+              node = d[i],
+              span = 3)
+  row <-length(rownames(co_occurrence_frequency))
+  for (j in 1:length(termRow)) {
+      for (k in 1:(row - 2)) {
+        if (d[j] == e$Term[k]) {
+          resultExtractionTable[i, j] <- e$Span[k] break
+        }
+      }
+    }
+}
 # 表の正規化
 # 確率行列化
 # PageRank計算
