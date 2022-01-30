d43fcb8d483c49b03ab6d41d2138fd05122f4160
asa-012 <yjdyj.20356299@gmail.com>
2022-01-31 02:21:15 +0900

    add:確率行列化

--- page_rank.R
+++ page_rank.R
@@ -5,12 +5,12 @@ sarukaniDF <-
         type = 1,
         pos = c("名詞", "形容詞", "動詞"))
 # 降順(行番号大→小)　４列目を並び替えたもの
-a <- sarukaniDF[order(sarukaniDF[, 4], decreasing = TRUE), ]
+a <- sarukaniDF[order(sarukaniDF[, 4], decreasing = TRUE),]
 # f以上のもののみを抽出しbに代入
-b <- a[a$"sarukani_gassen-utf8.txt" >= f, ]
+b <- a[a$"sarukani_gassen-utf8.txt" >= f,]
 c <-
   b[(b$POS2 != "非自立") &
-      (b$POS2 != "接尾") & (b$POS2 != "特殊") & (b$POS2 != "代名詞"), ]
+      (b$POS2 != "接尾") & (b$POS2 != "特殊") & (b$POS2 != "代名詞"),]
 termRow <- c$TERM
 resultExtractionTable <-
   matrix(0, nrow = length(termRow), ncol = length(termRow))
@@ -42,6 +42,26 @@ for (i in 1:length(termRow)) {
     h <- i
   }
 }
-P2 <- P1[-h, -h]
+P2 <- P1[-h,-h]
 # 確率行列化
+for (i in 1:ncol(P2)) {
+  for (j in 1:nrow(P2)) {
+    if (P2[i, j] != 0) {
+      P2[i, j] <-1
+    }
+  }
+}
+sumcol <- c()
+#各列の列和をsumcolに保存
+for (i in 1:ncol(P2)) {
+  sumcol <- c(sumcol, sum(P2[, i]))
+}
+for (i in 1:nrow(P2)) {
+  for (j in 1:ncol(P2)) {
+    if (P2[i, j] != 0) {
+      P2[i, j] <- P2[i, j] / sumcol[j]
+    }
+  }
+}
+
 # PageRank計算
